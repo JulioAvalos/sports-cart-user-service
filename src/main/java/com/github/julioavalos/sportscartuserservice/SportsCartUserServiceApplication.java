839a650644f2e25b202ea7e7e8c63aa46cf034f9
JulioAvalos <julioavalos2@msn.com>
2025-05-12 01:51:13 -0600

    first commit

---
+++ src/main/java/com/github/julioavalos/sportscartuserservice/SportsCartUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.julioavalos.sportscartuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsCartUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsCartUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/julioavalos/sportscartuserservice/SportsCartUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.julioavalos.sportscartuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsCartUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
